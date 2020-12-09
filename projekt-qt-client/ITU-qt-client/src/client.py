38756efd729574cad69f99d9dc81dfcd186345bb
scorpio11550@gmail.com <scorpio11550@gmail.com>
2020-12-10 00:18:08 +0100

    client class

--- projekt-qt-client/ITU-qt-client/src/client.py
+++ projekt-qt-client/ITU-qt-client/src/client.py
@@ -2,78 +2,82 @@
 # -*- coding: utf-8 -*-
 
 import json
-import src.error
+# from src.error import eprint
+from error import eprint
 import requests
-
-address = "0.0.0.0"
-port = "60606"
-address += ":" + port
 
 
 class Client:
     def __init__(self):
+        self.address = "http://localhost:60606"
+        self.code    = None
         self.session = requests.Session()
 
     def connect(self, username, password):
         return self.login(username, password)
 
-    # Private methods
     def __check_response(self, response):
         if response is None:
             eprint("No response received.")
+            self.code = None
             return False
-        if response.status_code != 200:
-            eprint("Bad response. Code: " + response.status_code)
+        if response.status_code >= 500:
+            eprint("Server error: " + str(response.status_code))
+            self.code = response.status_code
             return False
-        succesful = json.loads(response.json())
-        if not succesful["success"]:
-            eprint("Request was not succesful.")
+        if response.status_code >= 400:
+            eprint("Client error: " + str(response.status_code))
+            self.code = response.status_code
             return False
+
+        self.code = response.status_code
         return True
 
-    # Public methods
     def start_timer(self, time, action, script):
-        data = {  # TODO ked tak neviem co dostanem
-            "time": time,
-            "action": action,
-            "script": script
-        }
-        response = self.session.post(address + "/api/timer/start", data=data, timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/timer/start"
+        data = {"time": time, "action": action, "script": script}
+        response = self.session.post(address, json=json.dumps(data), timeout=5)
+        return True if self.__check_response(response) else False
 
     def stop_timer(self):
-        response = self.session.get(address + "/api/timer/stop", timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/timer/stop"
+        response = self.session.get(address, timeout=5)
+        return True if self.__check_response(response) else False
 
     def stat_timer(self):
-        response = self.session.get(address + "/api/timer/status", timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/timer/status"
+        response = self.session.get(address, timeout=5)
+        return True if self.__check_response(response) else False
 
     def start_monitor(self, time, action, resource):
-        data = {  # TODO ked tak neviem co dostanem
-            "time": time,
-            "action": action,
-            "resource": resource
-        }
-        response = self.session.post(address + "/api/monitor/start", data=data, timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/monitor/start"
+        data = {"time": time, "action": action, "resource": resource}
+        response = self.session.post(address, json=json.dumps(data), timeout=5)
+        return True if self.__check_response(response) else False
 
     def stop_monitor(self):
-        response = self.session.get(address + "/api/monitor/stop", timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/monitor/stop"
+        response = self.session.get(address, timeout=5)
+        return True if self.__check_response(response) else False
 
     def stat_monitor(self):
-        response = self.session.get(address + "/api/monitor/status", timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/monitor/status"
+        response = self.session.get(address, timeout=5)
+        return True if self.__check_response(response) else False
 
     def login(self, username, password):
-        data = {
-            "login": username,
-            "password": password
-        }
-        response = self.session.post(address + "/api/login", data=data, timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/login"
+        data = {"login": username, "password": password}
+        response = self.session.post(address, json=json.dumps(data), timeout=5)
+        return True if self.__check_response(response) else False
 
     def logout(self):
-        response = self.session.get(address + "/api/logout", timeout=5)
-        return True if __check_response(response) else False
+        address = self.address + "/api/logout"
+        response = self.session.get(address, timeout=5)
+        return True if self.__check_response(response) else False
+
+
+if __name__ == "__main__":
+    client = Client()
+    result = client.connect("tester", "12345")
+    print(result)
